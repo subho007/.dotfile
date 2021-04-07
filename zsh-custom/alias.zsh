759aa1bdcbf7917e5d215ffec96bba68df412f06
Subho Halder <subho.halder@gmail.com>
2021-04-07 13:26:38 +0530

    Added few useful aliases

--- zsh-custom/alias.zsh
+++ zsh-custom/alias.zsh
@@ -1,2 +1,10 @@
 alias git=hub
 alias g=git
+
+alias external_ip="curl -s icanhazip.com"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+# On the rare occasions I don't want to continue an interrupted download
+# I can always delete the incomplete fragment explicitly. I usually just
+# want to complete it.
+alias wget="wget -c"
