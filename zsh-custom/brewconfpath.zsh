67f9d25da3ad5d416677f70dd0d460d161ac730d
Subho Halder <subho.halder@gmail.com>
2025-01-03 01:05:02 +0400

    Update brew configs to support osx with arm

--- zsh-custom/brewconfpath.zsh
+++ zsh-custom/brewconfpath.zsh
@@ -19,41 +19,43 @@ if [[ -d "/usr/local/opt/make/libexec" ]]; then
     export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
 fi
 
+if [[ -d "/opt/homebrew/opt/make/libexec" ]]; then
+    export PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
+fi
+
 if [[ -d "/usr/local/opt/openssl@1.1/bin" ]]; then
     export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+fi
+
+if [[ -d "/opt/homebrew/opt/openssl@3/bin" ]]; then
+    export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
 fi
 
 if [[ -d "/usr/local/opt/sqlite/bin" ]]; then
     export PATH="/usr/local/opt/sqlite/bin:$PATH"
 fi
 
-if [[ -d "/usr/local/opt/m4/bin" ]]; then
-    export PATH="/usr/local/opt/m4/bin:$PATH"
+if [[ -d "/opt/homebrew/opt/sqlite/bin" ]]; then
+    export PATH="/opt/homebrew/opt/sqlite/bin:$PATH"
 fi
 
-if [[ -d "/usr/local/share/guile/site/3.0" ]]; then
-    export GUILE_LOAD_PATH="/usr/local/share/guile/site/3.0"
-    export GUILE_LOAD_COMPILED_PATH="/usr/local/lib/guile/3.0/site-ccache"
-    export GUILE_SYSTEM_EXTENSIONS_PATH="/usr/local/lib/guile/3.0/extensions"
+if [[ -d "/usr/local/opt/m4/bin" ]]; then
+    export PATH="/usr/local/opt/m4/bin:$PATH"
 fi
 
 if [[ -d "/usr/local/opt/openjdk/bin" ]]; then
     export PATH="/usr/local/opt/openjdk/bin:$PATH"
 fi
 
-if [[ -d "/usr/local/opt/texinfo/bin" ]]; then
-    export PATH="/usr/local/opt/texinfo/bin:$PATH"
-fi
-
-if [[ -d "/usr/local/opt/bzip2/bin" ]]; then
-    export PATH="/usr/local/opt/bzip2/bin:$PATH"
+if [[ -d "/opt/homebrew/opt/openjdk/bin" ]]; then
+    export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 fi
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-[[ /usr/local/bin/fnm ]] && eval "$(fnm env)"
+command -v fnm >/dev/null 2>&1 && eval "$(fnm env)"
 
-if [[ /usr/local/bin/pyenv ]]; then
+if command -v pyenv >/dev/null 2>&1; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
     # load the pyenv to PATH
@@ -63,18 +65,9 @@ if [[ /usr/local/bin/pyenv ]]; then
     export AUTOSWITCH_DEFAULT_PYTHON="$(which python3)"
 fi
 
-if [[ -f "/usr/local/bin/go" ]]; then
+if [[ -f "/usr/local/bin/go" ]] || [[ -f "/opt/homebrew/bin/go" ]] then
     export PATH=$PATH:$(go env GOPATH)/bin
     export GOPATH=$(go env GOPATH)
-fi
-
-if [[ -f "/usr/local/opt/postgresql@13/bin/psql" ]]; then
-    export PATH="/usr/local/opt/postgresql@13/bin:$PATH"
-fi
-
-if [[ -d "/usr/local/share/android-ndk" ]]; then
-    export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
-    export ANDROID_NDK_ROOT=$ANDROID_NDK_HOME
 fi
 
 #  Template below
@@ -83,12 +76,9 @@ fi
 # fi
 
 
-export LDFLAGS="-L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib"
-export CPPFLAGS="-I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include"
-
-if [[ -f "/usr/local/bin/zoxide" ]]; then
+if [[ -f "/usr/local/bin/zoxide" ]] || [[ -f "/opt/homebrew/bin/zoxide" ]] then
     autoload -Uz compinit
     eval "$(zoxide init zsh --no-cmd --cmd cd)"
 fi
 
-export DOTNET_ROOT="/usr/local/opt/dotnet/libexec"
+export DOTNET_ROOT="/opt/homebrew/opt/dotnet/libexec"
